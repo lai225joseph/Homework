9db979be1aa23019c93da72f3875639958e30a33
Kwei-Gang Lai <lai225joseph@hotmail.com>
2019-08-14 21:00:27 +0800

    Test

---
+++ Homework.Test/UserTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Homework.Domain;
+using Homework.Service;
+using Homework.Repository;
+
+namespace Homework.Test
+{
+    [TestClass]
+    public class UserTest
+    {
+        private UserContext db = new UserContext();
+
+        [TestMethod]
+        public void TestCreate()
+        {
+            User user = new User();
+            user.UserID = Guid.NewGuid();
+            user.UserName = "jjj";
+            user.Passwork = "jjj";
+            user.Birthday = DateTime.Today;
+            user.UserEmailAddress = "111@mail.com";
+            db.User.Add(user);
+            db.SaveChanges();
+        }
+    }
+}
