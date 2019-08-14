7b02513b1c2aae1e3b2730fb5be7b4bb5fb50351
Kwei-Gang Lai <lai225joseph@hotmail.com>
2019-08-14 20:13:10 +0800

    service add entityframework

---
+++ Homework.Service/UserContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Homework.Domain;
+
+namespace Homework.Service
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(): base("UserContext")
+        {
+            Database.SetInitializer<UserContext>(null);
+            Configuration.AutoDetectChangesEnabled = true;
+            Configuration.LazyLoadingEnabled = true;
+            Configuration.ProxyCreationEnabled = true;
+            Configuration.ValidateOnSaveEnabled = true;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Configuration.LazyLoadingEnabled = false;
+            base.Dispose(disposing);
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
