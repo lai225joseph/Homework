919a51a9c6ff6b65ac19fcc23638972a0f079bc5
Kwei-Gang Lai <lai225joseph@hotmail.com>
2019-08-14 20:22:28 +0800

    fundamental CRUD application

---
+++ Homework.Repository/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Homework.Service;
+using Homework.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Repository
+{
+    public class UserRepository
+    {
+    }
+}
